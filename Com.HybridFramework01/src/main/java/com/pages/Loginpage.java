c46132cf13c51ab5581ab24b9c1b23e1447e7029
Admin <Admin@Admin-PC>
2022-10-08 22:47:31 +0530

    Added new HYBproject

---
+++ Com.HybridFramework01/src/main/java/com/pages/Loginpage.java
@@ -0,0 +1,34 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.base.BaseClass;
+
+public class Loginpage extends BaseClass {
+	
+	@FindBy(id="email")
+	WebElement uname;
+	
+	@FindBy(id="password")
+	WebElement pass;
+
+	@FindBy(id="//button")
+	WebElement loginBtn;
+	
+	public Loginpage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void logintoApplication(String username, String password) {
+		
+		uname.sendKeys(username);
+		pass.sendKeys(password);
+		loginBtn.click();
+		
+		
+		
+	}
+}
